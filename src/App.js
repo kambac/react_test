2f37e900d5c38e4748efc6a40c2cb730c134a590
kambac3 <kambac3@wp.pl>
2021-02-01 22:20:22 +0100

    remove checkbox instance

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
             <Header />
             <Checkbox />
             <Checkbox />
-            <Checkbox />
             <Contacts />
             <Footer />
         </div>
